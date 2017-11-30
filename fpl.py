6ec93da1bc0219059768db833b1610df7431ce87
Kinley Tshering <kxt5258@gmail.com>
2017-11-30 11:43:51 +0600

    captain check even for benched players

--- fpl.py
+++ fpl.py
@@ -1,3 +1,4 @@
+
 import sys
 import requests
 from tabulate import tabulate
@@ -15,6 +16,7 @@ else:
     else:
         pass
 
+print("Getting team and player data")
 entire_data = requests.get('https://fantasy.premierleague.com/drf/bootstrap-static')
 player_data = entire_data.json()['elements']
 teams_data = entire_data.json()['teams']
@@ -28,10 +30,12 @@ for a in player_data:
     row = {"name": a['web_name'], 'team': teams[a['team_code']]}
     players[a['id']] = row
 
+print("Getting group data")
 r = requests.get('https://fantasy.premierleague.com/drf/leagues-classic-standings/%s' % str(sys.argv[1]))
 group_data = r.json()
 in_data = group_data['standings']['results']
 
+print("Getting squad picks data")
 group_data = {}
 winner = {"point": 0, "player": []}
 for a in in_data:
@@ -49,19 +53,24 @@ for a in in_data:
                     row['vice_captain'] = b['element']
             else:
                 row['bench'].append(b['element'])
+                if b['is_captain']:
+                    row['captain'] = b['element']
+                if b['is_vice_captain']:
+                    row['vice_captain'] = b['element']
+
         row['active_chip'] = data['active_chip']
         row['points'] = data['entry_history']['points']
         row['total_points'] = data['entry_history']['total_points']
         row['transfer_cost'] = data['entry_history']['event_transfers_cost']
         row['winner'] = ''
-        actual_point = row['points'] - row['transfer_cost']
+        row['actual_point'] = row['points'] - row['transfer_cost']
 
         # Look for highest point
-        if actual_point > 0:
-            if winner['point'] == actual_point:
+        if row['actual_point'] > 0:
+            if winner['point'] == row['actual_point']:
                 winner['player'].append(a['entry'])
-            elif winner['point'] < actual_point:
-                winner['point'] = actual_point
+            elif winner['point'] < row['actual_point']:
+                winner['point'] = row['actual_point']
                 winner['player'] = [a['entry']]
             else:
                 pass
@@ -81,8 +90,8 @@ for member in group_data:
     a = group_data[member]
     row = [a['manager'], a['player_name'], players[a['captain']]['name'],
            players[a['vice_captain']]['name'], a['active_chip'], a['points'],
-           a['transfer_cost'], a['points'] - a['transfer_cost'], a['winner']]
+           a['transfer_cost'], a['actual_point'], a['winner'], a['total_points']]
     data.append(row)
 
-print(tabulate(data, ['Manager', 'Name', 'Captain', 'Vice Captain', 'Chips',
-                      'GW Pts', 'Xfer Pts', 'Point', 'Winner']))
+print(tabulate(data, ['Team', 'Manager', 'Captain', 'Vice Captain', 'Chips',
+                      'GW Pts', 'Xfer Pts', 'Point', 'Winner', 'Total Pts']))
